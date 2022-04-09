050ebc0afcdba33f5388f74639fc24e6d01f341c
Nelson Lojo <nalsonklojo@gmail.com>
2022-04-09 12:49:16 -0700

    shell out to buildctl per tag under [[build]] in .tranpsire.toml

--- transpire/cli/image.py
+++ transpire/cli/image.py
@@ -1,5 +1,7 @@
 import click
-
+from shutil import which as shutil_which
+from tomlkit import parse as parse_toml
+from subprocess import run, PIPE
 
 @click.group()
 def commands(**kwargs):
@@ -10,7 +12,42 @@ def commands(**kwargs):
 @commands.command()
 def build(**kwargs):
     """build defined images"""
-    raise NotImplementedError("Not yet implemented!")
+
+    # ensure we have buildkit installed
+    if shutil_which("buildctl") is None:
+        raise RuntimeError("You must install Buildkit to use this script.")
+
+    # get remote and additional tags (like one by commit)
+    remote: str  = kwargs.get('registry', None)
+    tags: list = kwargs.get('additional_tags', [])
+
+    # read the dockerfile path, app name, and tags
+    with open(f".transpire.toml", 'r') as tomlFile:
+        config = parse_toml(tomlFile.read())
+        
+        build_data = config['build'][0]
+
+        dockerfile: str = f"./{build_data['dockerfile']}"
+        name: str = build_data['name']
+        tags.extend(build_data['tags'])
+    
+    repo: str = f"{remote}" + (f'/{name}' if name is not None else '')
+    
+    for tag in tags:
+        build_dest: str = f"{repo}:{tag}"    
+        
+        buildArgs = [
+            "builtctl", "build",
+            "--frontend", "dockerfile.v0",
+            "--local", f"dockerfile={dockerfile}",
+            "--opt", "",
+            "--output", 
+                "type=image," + \
+                f"name={build_dest}," + \
+                "push=true" # not sure about this
+        ]
+
+        buildOutput = run(buildArgs, check=True, stdout=PIPE).stdout
 
 
 @commands.command()
@@ -23,3 +60,31 @@ def push(**kwargs):
 def list(**kwargs):
     """list defined images"""
     raise NotImplementedError("Not yet implemented!")
+
+"""
+[[build]]
+name = "webserver"
+tags = ["v1.0"]
+dockerfile = "Dockerfile"
+
+[[test]]
+name = "e2e_tests"
+dockerfile = "test.Dockerfile"
+depends_on = ["webserver"]
+"""
+{
+    'build': [
+        {
+            'name': 'webserver', 
+            'tags': ['v1.0'], 
+            'dockerfile': 'Dockerfile'
+        }
+    ], 
+    'test': [
+        {
+            'name': 'e2e_tests', 
+            'dockerfile': 'test.Dockerfile', 
+            'depends_on': ['webserver']
+        }
+    ]
+}
