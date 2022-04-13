a16dbb10bbbeaf79d458c53399b370562dbf4bbd
nelcel <nalsonklojo@gmail.com>
2022-04-13 13:00:37 -0700

    suggestions integrated

--- transpire/helpers/helm.py
+++ transpire/helpers/helm.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import os
 import shutil
 import tempfile
+from textwrap import indent
 from subprocess import PIPE
 from subprocess import run
-from typing import Any
+from typing import Any, List
 
 import yaml
 
@@ -96,33 +97,43 @@ def build_chart(
         raise RuntimeError("You must install Helm to use this script.")
 
     # make sure the current user has a place for us to cache stuff
-    home_cache = os.environ.get('XDG_CACHE_HOME', None)
-    if not home_cache:
-        raise EnvironmentError("Could not find an XDG_CACHE_HOME environment variable")
-    cache_dir = os.path.join(home_cache, 'ocf_helm')
+    if (cache_dir := os.environ.get("TRANSPIRE_CACHE_DIR", None)) is not None:
+        pass
+    elif (cache_dir := os.environ.get("XDG_CACHE_HOME", None)) is not None:
+        cache_dir = os.path.join(cache_dir, "transpire")
+    elif (cache_dir := os.environ.get("HOME", None)) is not None:
+        cache_dir = os.path.join(cache_dir, ".cache", "transpire")
+    else:
+        cache_dir = os.path.join("/tmp", "transpire")
 
     if not os.path.isdir(cache_dir):
         os.mkdir(cache_dir)
 
+    chart_dir: str = os.path.join(cache_dir, "charts")
+
     # if the compressed chart is in our cache and it's of the right version
-    archive_name = f"{name}-{version}.tgz"
-    chart_exists = os.path.isfile(os.path.join(cache_dir, archive_name))
+    archive_name: str = f"{name}-{version}.tgz"
+    chart_exists: bool = os.path.isfile(os.path.join(chart_dir, archive_name))
 
     # if we don't have the chart, download it (as an archive)
     if not chart_exists:
-        pull_args = [
+        pull_args: List[str] = [
             "helm",
             "pull",
-            "-d", cache_dir,
+            "-d", chart_dir,
             "--version", version,
             "--repo", repo_url,
             chart_name
         ]
-        should_be_empty = run(
+        should_be_empty: bytes = run(
             pull_args, 
             check=True, 
             stderr=PIPE,
         ).stderr
+
+        if len(should_be_empty) > 0:
+            err_msg: str = indent(str(should_be_empty), "  ")
+            raise RuntimeError(f"Error when downloading chart archive: \n{err_msg}")
 
     values_file, values_file_name = tempfile.mkstemp(suffix=".yml")
     with open(values_file_name, "w") as f:
