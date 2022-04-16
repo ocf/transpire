789f1e24991e2737f8f83d545245212d874ae95b
Nikhil Jha <hi@nikhiljha.com>
2022-04-16 12:18:09 -0700

    style: don't str(bool)

--- transpire/cli/image.py
+++ transpire/cli/image.py
@@ -4,6 +4,7 @@ from tomlkit import parse as parse_toml
 from subprocess import run, PIPE
 from typing import List
 
+
 @click.group()
 def commands(**kwargs):
     """tools related to images (.transpire.toml)"""
@@ -22,47 +23,47 @@ def build(push, **kwargs):
         raise RuntimeError("You must install Buildkit to use this script.")
 
     # get remote and additional tags (like one by commit)
-    remote: str = kwargs.get('remote', None)
-    tags: List[str] = kwargs.get('additional_tags', [])
+    remote: str = kwargs.get("remote", None)
+    tags: List[str] = kwargs.get("additional_tags", [])
 
     if (push is None) and (remote is None):
         raise ValueError(f"Argument remote cannot be None when --push is set")
 
     # read the dockerfile path, app name, tags, context
-    with open(f".transpire.toml", 'r') as tomlFile:
+    with open(f".transpire.toml", "r") as tomlFile:
         config = parse_toml(tomlFile.read())
-        
+
     build_output: List[bytes] = []
-    for build_data in config['build']:
+    for build_data in config["build"]:
 
         dockerfile: str = f"./{build_data['dockerfile']}"
-        name: str = build_data['name']
-        tags.extend(build_data['tags'])
-        build_context: str = build_data.get('context', '.')
-    
+        name: str = build_data["name"]
+        tags.extend(build_data["tags"])
+        build_context: str = build_data.get("context", ".")
+
         # format the remote location to push to: "{remote}", "{remote}/{name}" or "{name}"
-        repo: str = '/'.join(filter(lambda arg: arg is not None, [remote, name]))
+        repo: str = "/".join(filter(lambda arg: arg is not None, [remote, name]))
         if len(repo) < 1:
             raise Exception(f"Build target has no identifier: \n{build_data}")
 
         # build and push the image to each tag on that remote
         for tag in tags:
-            build_dest: str = f"{repo}:{tag}"    
-            
+            build_dest: str = f"{repo}:{tag}"
+
             build_args = [
-                "buildctl", "build",
-                "--frontend", "dockerfile.v0",
-                "--local", f"context={build_context}", 
-                "--local", f"dockerfile={dockerfile}",
-                "--output", 
-                    "type=image," + \
-                    f"name={build_dest}," + \
-                    f"push={str(push).lower()}"
+                "buildctl",
+                "build",
+                "--frontend",
+                "dockerfile.v0",
+                "--local",
+                f"context={build_context}",
+                "--local",
+                f"dockerfile={dockerfile}",
+                "--output",
+                "type=image," + f"name={build_dest}," + f"push={'true' if push else 'false'}",
             ]
 
-            build_output.append(
-                run(build_args, check=True, stdout=PIPE).stdout
-            )
+            build_output.append(run(build_args, check=True, stdout=PIPE).stdout)
 
 
 @commands.command()
