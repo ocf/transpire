0cc49647993a8d3c8522b20605d64b691a78e0b9
Oliver Ni <oliver.ni@gmail.com>
2022-12-09 01:58:51 -0800

    Add image init

--- transpire/types.py
+++ transpire/types.py
@@ -38,6 +38,10 @@ class Image:
     name: str
     path: Path
 
+    def __init__(self, name, path):
+        self.name = name
+        self.path = Path(path)
+
     @cached_property
     def resolved_path(self):
         if self.path.is_absolute():
