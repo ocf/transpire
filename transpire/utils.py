3cc7b12ab401423d9ee793d2dc8c7550b31e7a9c
Nikhil Jha <nikhiljha@users.noreply.github.com>
2023-01-15 16:41:48 -0800

    chore: move imports to top of file in utils.py

--- transpire/utils.py
+++ transpire/utils.py
@@ -1,10 +1,9 @@
+import pathlib
 import tomllib
 from typing import Dict, cast
 
 
 def get_versions(caller) -> Dict[str, Dict[str, str]]:
-    import pathlib
-
     file = pathlib.Path(caller).parent.resolve() / "versions.toml"
 
     return cast(
