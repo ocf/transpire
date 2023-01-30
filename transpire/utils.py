56b191c28dc7b1648a02d4f9e9c4b36085455b4c
Nikhil Jha <nikhiljha@users.noreply.github.com>
2023-01-30 10:59:38 -0800

    feat: util to grab any adjacent file

--- transpire/utils.py
+++ transpire/utils.py
@@ -3,8 +3,21 @@ import tomllib
 from typing import Dict, cast
 
 
+def get_file(caller, filename: str) -> pathlib.Path:
+    """
+    Given the __file__ parameter of the calling file and
+    the filename of an adjacent file, return the path to
+    that file.
+    """
+    return pathlib.Path(caller).parent.resolve() / filename
+
+
 def get_versions(caller) -> Dict[str, Dict[str, str]]:
-    file = pathlib.Path(caller).parent.resolve() / "versions.toml"
+    """
+    Given the __file__ parameter of the calling file, return
+    the contents of versions.toml cast to a dict.
+    """
+    file = get_file(caller, "versions.toml")
 
     return cast(
         Dict[str, Dict[str, str]],
