4b6e681ac824528e5975fc7a2e2dd2c008c67cd0
Oliver Ni <oliverni@berkeley.edu>
2024-07-07 15:15:32 -0400

    feat: expose pvc

--- transpire/resources/__init__.py
+++ transpire/resources/__init__.py
@@ -1,8 +1,9 @@
 from .configmap import ConfigMap
 from .deployment import Deployment
 from .ingress import Ingress
+from .pvc import PersistentVolumeClaim
 from .secret import Secret
 from .service import Service
 from .statefulset import StatefulSet
 
-__all__ = ["Deployment", "Ingress", "Service", "Secret", "ConfigMap"]
+__all__ = ["Deployment", "Ingress", "Service", "Secret", "ConfigMap", "PersistentVolumeClaim"]
